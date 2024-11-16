67a598d80cfa9eefa4afbeaad543ddd769639aa9
Laurel Wood <laureljohne@gmail.com>
2024-11-16 17:05:32 -0500

    adjusted user service to add gets

--- pangolin-app/pangolin/src/users/users.service.ts
+++ pangolin-app/pangolin/src/users/users.service.ts
@@ -1,6 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from './users';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-    constructor(){}
+    constructor(@InjectRepository(Users) private repo: Repository<Users>){};
+
+    async getAllUsers(): Promise<Users[]> {
+        return await this.repo.find({
+            relations: {
+                reports: true
+            }
+        });
+    };
+
+    async getUserById(idToFind: number): Promise<Users>{
+        return await this.repo.findOneOrFail({
+            where: {
+                user_id: idToFind
+            },
+            relations: {
+                reports: true
+            }
+        }).catch(()=>{
+            throw new HttpException(`User with Id ${idToFind} does not exist`, HttpStatus.NOT_FOUND)
+        })
+
+    }
+    
+
+
+
+
 }
