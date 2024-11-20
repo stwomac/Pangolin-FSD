071bcb8e3e7d4dc9e8f9a2af4af8e6c3e4a42a93
jeffreymeatcutter <jeffreymeatcutter@gmail.com>
2024-11-20 08:46:40 -0500

    added imports to context service, tested Amazon RDS integration

--- pangolin-app/pangolin/src/context/context.service.ts
+++ pangolin-app/pangolin/src/context/context.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DeleteResult } from 'typeorm';
+import { Context } from './context';
 
 @Injectable()
 export class ContextService {
