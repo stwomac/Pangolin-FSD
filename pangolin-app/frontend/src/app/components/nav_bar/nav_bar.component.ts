cb6a53ee2fc79f151c8713654c2a3e8c6640b918
Laurel Wood <laureljohne@gmail.com>
2024-11-25 12:25:31 -0500

    adding nav bar

---
+++ pangolin-app/frontend/src/app/components/nav_bar/nav_bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-nav_bar',
+  imports: [],
+  templateUrl: './nav_bar.component.html',
+  styleUrl: './nav_bar.component.css',
+})
+export class Nav_BarComponent {}
