2f50a44f449b5f7a92bae2bf46669bee700dfc1f
Ben Keys <bkeys818@gmail.com>
2024-11-25 15:27:39 -0500

    🔥 Remove context-type/:id endpoint

--- pangolin-app/api/src/context_type/context_type.controller.ts
+++ pangolin-app/api/src/context_type/context_type.controller.ts
@@ -6,17 +6,8 @@ import { ContextType } from './context_type'
 export class ContextTypeController {
   constructor(private readonly contextTypeService: ContextTypeService) {}
 
-  // //get all
   @Get()
-  @HttpCode(200)
   getAllContextTypes(): Promise<ContextType[]> {
     return this.contextTypeService.getAllContextTypes()
-  }
-
-  // get by ID
-  @Get(':id')
-  @HttpCode(200)
-  getCourseById(@Param('id') idToFind: number): Promise<ContextType> {
-    return this.contextTypeService.getContextTypeById(idToFind)
   }
 }
--- pangolin-app/api/src/context_type/context_type.service.ts
+++ pangolin-app/api/src/context_type/context_type.service.ts
@@ -11,29 +11,6 @@ export class ContextTypeService {
 
   //get all context type
   async getAllContextTypes(): Promise<ContextType[]> {
-    return await this.repo.find({
-      relations: {
-        contexts: true, // Include the related contexts
-      },
-    })
-  }
-
-  // get by ID
-  async getContextTypeById(idToFind: number): Promise<ContextType> {
-    return await this.repo
-      .findOneOrFail({
-        where: {
-          contextTypeId: idToFind,
-        },
-        relations: {
-          contexts: true, // Include the related contexts
-        },
-      })
-      .catch(() => {
-        throw new HttpException(
-          `ContextType with context_type_id ${idToFind} does not exist!`,
-          HttpStatus.NOT_FOUND,
-        )
-      })
+    return await this.repo.find()
   }
 }
