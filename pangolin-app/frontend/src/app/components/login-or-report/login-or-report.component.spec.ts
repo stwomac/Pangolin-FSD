e1d940a34d2198b24260a93d1732c651a22c15bd
womack <womack.st9@gmail.com>
2024-11-25 17:50:08 -0600

    view1 component merge

---
+++ pangolin-app/frontend/src/app/components/login-or-report/login-or-report.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginOrReportComponent } from './login-or-report.component';
+
+describe('LoginOrReportComponent', () => {
+  let component: LoginOrReportComponent;
+  let fixture: ComponentFixture<LoginOrReportComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LoginOrReportComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginOrReportComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
