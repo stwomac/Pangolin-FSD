d8e8a37bd3de42c972ffc853d3035255b096798a
womack <womack.st9@gmail.com>
2024-11-25 16:25:44 -0600

    html and css for view 1

--- pangolin-app/frontend/src/app/app.routes.ts
+++ pangolin-app/frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router'
+import { Routes } from '@angular/router';
+import { LoginOrReportComponent } from './components/login-or-report/login-or-report.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+    {
+        path: 'view1',
+        component: LoginOrReportComponent
+    }
+]
