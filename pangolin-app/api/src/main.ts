adc110a86dfb8a20ca368f7ebbcaae6c054e1747
Ben Keys <bkeys818@gmail.com>
2024-11-26 17:05:01 -0500

    🔧 Configure CORS

--- pangolin-app/api/src/main.ts
+++ pangolin-app/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({ origin: process.env.TRUSTED_ORIGINS })
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
