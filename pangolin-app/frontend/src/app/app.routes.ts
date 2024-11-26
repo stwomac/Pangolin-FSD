907c7d76a6a9b0f5c4340feb70268b5f750a30a4
jeffreymeatcutter <jeffreymeatcutter@gmail.com>
2024-11-26 10:56:29 -0500

    router for create report component

--- pangolin-app/frontend/src/app/app.routes.ts
+++ pangolin-app/frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router'
 import { LoginOrReportComponent } from '../app/components/login-or-report/login-or-report.component' 
+import { CreateReportComponent } from './components/createReport/create-report.component'
 
 export const routes: Routes = [
 
     {
         path: 'view1',
         component: LoginOrReportComponent
+    },
+    {
+        path: 'create-report',
+        component: CreateReportComponent
     }
 ]
