e5388ffb47eb3c67aa0f5a6ffeb9894e5808b499
DESKTOP-MGJPBE2\paule <pauledson98@yahoo.com>
2024-11-26 14:18:22 -0600

    Merge branch 'main' of https://github.com/stwomac/Pangolin-FSD

--- pangolin-app/frontend/src/app/components/report/report.component.ts
+++ pangolin-app/frontend/src/app/components/report/report.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { PaymentMethod, Reports } from '../../models/reports';
+import { Users } from '../../models/users';
+import { ReportType } from '../../models/context-type';
+
+let user: Users
+let reportType: ReportType
+let paymentMethod: PaymentMethod
+let date: Date
 
 @Component({
   selector: 'app-report',
-  imports: [],
+  imports: [Reports, Users],
   templateUrl: './report.component.html',
   styleUrl: './report.component.css',
 })
-export class ReportComponent {}
+export class ReportComponent {
+  @Input() report: Reports = new Reports(0, user , reportType, '', false, '', paymentMethod, date, date, false);
+}
