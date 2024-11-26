f443422d208a7f938d81fe0c756af0974bd81dc3
Laurel Wood <laureljohne@gmail.com>
2024-11-26 14:26:39 -0500

    nav_bar adjustment

--- pangolin-app/frontend/src/app/app.component.ts
+++ pangolin-app/frontend/src/app/app.component.ts
@@ -4,10 +4,12 @@ import { FooterComponent } from './components/footer/footer.component'
 import { HeaderComponent } from './components/header/header.component'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { LoginComponent } from './components/login/login.component';
+import { Nav_BarComponent } from './components/nav_bar/nav_bar.component'
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, MatSlideToggleModule, LoginComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, MatSlideToggleModule, LoginComponent, Nav_BarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
