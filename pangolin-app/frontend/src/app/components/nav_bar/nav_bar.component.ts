606e3d6a349d0e7693ea9a2672b0aff09bc4352c
Laurel Wood <laureljohne@gmail.com>
2024-11-26 17:08:10 -0500

    started nav_bar routing

--- pangolin-app/frontend/src/app/components/nav_bar/nav_bar.component.ts
+++ pangolin-app/frontend/src/app/components/nav_bar/nav_bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav_bar',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core'
   templateUrl: './nav_bar.component.html',
   styleUrl: './nav_bar.component.css',
 })
-export class Nav_BarComponent {}
+export class Nav_BarComponent {
+
+  constructor(private router: Router) {};
+
+  navigateToEntry(){
+    this.router.navigate(['/view1'])
+  }
+
+  navigateToSignIn(){
+    let isAuth= true;
+    if(isAuth)
+      this.router.navigate(['/report'])
+    else
+      this.router.navigate(['/view1'])
+  }
+
+}
