c5d8cd333f04dabe72d4be909aebc33dfff96a01
Ben Keys <bkeys818@gmail.com>
2024-11-27 10:31:47 -0500

    🚑 Fix database table names

--- pangolin-app/api/src/user/user.ts
+++ pangolin-app/api/src/user/user.ts
@@ -1,7 +1,7 @@
 import { Report } from 'src/report/report'
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
 
-@Entity()
+@Entity({ name: 'person' })
 export class User {
   @PrimaryGeneratedColumn()
   userId: number
