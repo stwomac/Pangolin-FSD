5b0ad65f0f8b8af2bad0f2cbf7d549853df432a4
DESKTOP-MGJPBE2\paule <pauledson98@yahoo.com>
2024-11-27 09:52:13 -0600

    push before merge

--- pangolin-app/frontend/src/app/components/report/report.component.ts
+++ pangolin-app/frontend/src/app/components/report/report.component.ts
@@ -1,39 +1,40 @@
 import { Component, Input } from '@angular/core'
-import { Report, ReportLike, ReportType, User, Annotation, PaymentMethod } from '../../models/reports';
+import { Report, ReportLike, ReportType, User, Annotation, PaymentMethod } from '../../models/report';
 import { Context } from '../../models/context';
 import { ReportServices } from '../../services/report.service';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
+import { MatButton } from '@angular/material/button';
 //import { Users } from '../../models/users';
 //import { ReportType } from '../../models/context-type';
 let reportlike :ReportLike
 let user:User
 let reportType:ReportType
-let annotation:Annotation
+let annotation:Annotation[]
 let paymentMethod:PaymentMethod
-let contexts:Context
+let contexts:Context[]
 @Component({
   selector: 'app-report',
-  imports: [MatCardModule, MatChipsModule],
+  imports: [MatCardModule, MatChipsModule, MatButton],
   templateUrl: './report.component.html',
   styleUrl: './report.component.css',
   standalone: true
 })
 export class ReportComponent {
   message:any;
-  @Input() report: ReportLike = {
-    reportId: 1,
-    reportee: user,
-    reportType: reportType,
-    description: 'Report',
-    paid: false,
-    amount: '',
-    paymentMethod: paymentMethod,
-    isSus: false,
-    isDone: false,
-    annotations: annotation,
-    contexts: contexts
-  }
+   @Input() report: ReportLike = {
+     reportId: 1,
+     reportee: user,
+     reportType: reportType,
+     description: 'Report',
+     paid: false,
+     amount: '',
+     paymentMethod: paymentMethod,
+     isSus: false,
+     isDone: false,
+     annotations: annotation,
+     contexts: contexts
+   }
   constructor(private apiService: ReportServices) { };
    ngOnInit() {
      this.apiService.get(1).subscribe(data => {
