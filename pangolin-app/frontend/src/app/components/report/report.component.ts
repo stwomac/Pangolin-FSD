0a5093f2b52ccba71499dcb5206a3196a63714ad
DESKTOP-MGJPBE2\paule <pauledson98@yahoo.com>
2024-11-26 19:48:44 -0600

    report component mat-card testing

--- pangolin-app/frontend/src/app/components/report/report.component.ts
+++ pangolin-app/frontend/src/app/components/report/report.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core'
 import { Report, ReportLike, ReportType, User, Annotation, PaymentMethod } from '../../models/reports';
 import { Context } from '../../models/context';
 import { ReportServices } from '../../services/report.service';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
 //import { Users } from '../../models/users';
 //import { ReportType } from '../../models/context-type';
 let reportlike :ReportLike
@@ -12,7 +14,7 @@ let paymentMethod:PaymentMethod
 let contexts:Context
 @Component({
   selector: 'app-report',
-  imports: [],
+  imports: [MatCardModule, MatChipsModule],
   templateUrl: './report.component.html',
   styleUrl: './report.component.css',
   standalone: true
