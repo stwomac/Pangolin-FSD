022ba16f7834184c436ebaf017d8cab331609df0
Laurel Wood <laureljohne@gmail.com>
2024-11-29 12:53:41 -0500

    added/adjusted routing and CSS on view1 and nav_bar

--- pangolin-app/frontend/src/app/components/nav_bar/nav_bar.component.ts
+++ pangolin-app/frontend/src/app/components/nav_bar/nav_bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserServices } from '../../services/user.service';
 
 @Component({
   selector: 'app-nav_bar',
@@ -9,18 +10,21 @@ import { Router } from '@angular/router';
 })
 export class Nav_BarComponent {
 
-  constructor(private router: Router) {};
+  constructor(private router: Router, private userService : UserServices) {};
 
   navigateToEntry(){
     this.router.navigate(['/view1'])
   }
 
   navigateToSignIn(){
-    let isAuth= true;
-    if(isAuth)
-      this.router.navigate(['/report'])
-    else
-      this.router.navigate(['/view1'])
+
+    this.userService.isLoggedIn().subscribe(isLoggedIn => {
+      if (isLoggedIn) {
+        this.router.navigate(['/report'])
+      } else {
+        this.router.navigate(['/login'])
+      }
+    })
   }
 
 }
