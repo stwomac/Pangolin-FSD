6ec5beb1e3be5bc261d9e7b06daa515310d5f093
jeffreymeatcutter <jeffreymeatcutter@gmail.com>
2024-12-03 18:30:44 -0500

    finished feature!!

--- pangolin-app/frontend/src/app/components/report-description-editor/report-description-editor.component.ts
+++ pangolin-app/frontend/src/app/components/report-description-editor/report-description-editor.component.ts
@@ -1,13 +1,12 @@
 import {FormControl,ReactiveFormsModule} from '@angular/forms';
-import { Report } from '../models/report';
+import { Report } from '../../models/report';
 import { Component, Input } from '@angular/core';
-import {MatButton} from '@angular/material/button';
 import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
 import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-report-description-editor',
-  imports: [NgIf,MatButton,MatInput,ReactiveFormsModule,MatFormField,MatLabel],
+  imports: [NgIf,MatInput,ReactiveFormsModule,MatFormField,MatLabel],
   templateUrl: './report-description-editor.component.html',
   styleUrl: './report-description-editor.component.css'
 })
